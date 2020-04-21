38e95cb32c24c27eeeec1ab7e9da0d8cbb282e1d
mia-im <62039441+mia-im@users.noreply.github.com>
2020-04-21 22:47:10 +0300

    Update if.php

--- if.php
+++ if.php
@@ -12,19 +12,7 @@ elseif(empty($_SESSION['lang'])){
 	header("Location: language.php");
 }
 
-else switch ($_SESSION['role']) {
-	case 'admin':
-		header("Location: admin.php");
-		break;
-	
-	case 'client':
-		header("Location: client.php");
-		break;
-	
-	case 'manager':
-		header("Location: manager.php");
-		break;
-}
+else header("Location:".strtolower($_SESSION['role']).".php");
 ?>
 
 
